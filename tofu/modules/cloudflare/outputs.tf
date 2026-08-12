0dc97b1bec6777b865410b594024f441e0270023
fredrir <fhansteen@gmail.com>
2026-08-13 00:44:34 +0200

    tofu: fmt outputs.tf after the record rename (E5b)

--- tofu/modules/cloudflare/outputs.tf
+++ tofu/modules/cloudflare/outputs.tf
@@ -2,6 +2,6 @@ output "record_ids" {
   description = "DNS record ids by hostname/type, for audits against the zone listing."
   value = {
     llunde = { for h, r in cloudflare_dns_record.llunde_tunnel_cname : h => r.id }
-    cname = { for h, r in cloudflare_dns_record.tunnel_cname : h => r.id }
+    cname  = { for h, r in cloudflare_dns_record.tunnel_cname : h => r.id }
   }
 }
